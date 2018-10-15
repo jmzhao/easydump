46acc73d23b914a301b097678fec43985f4373bd
Jinman Zhao <jinman.zhao@gmail.com>
2018-10-15 13:32:07 -0500

    rearrange

---
+++ python/easydump/bin.py
@@ -0,0 +1,24 @@
+import struct
+
+struct_int = struct.Struct('i')
+struct_float = struct.Struct('d')
+
+class BINBaseLoader :
+    def load_int(self, _) :
+        return struct_int.unpack(self.stream.read(struct_int.size))[0]
+    def load_float(self, _) :
+        return struct_float.unpack(self.stream.read(struct_float.size))[0]
+
+class BINBaseDumper :
+    def dump_int(self, x) :
+        return self.stream.write(struct_int.pack(x))
+    def dump_float(self, x) :
+        return self.stream.write(struct_float.pack(x))
+
+class BINLoader (Loader, CompactLoading, BINBaseLoader) :
+    def __init__(self, fp, stream_wrapper = None) :
+        self.stream = stream_wrapper(fp) if stream_wrapper else fp
+
+class BINDumper (Dumper, CompactDumping, BINBaseDumper) :
+    def __init__(self, fp, stream_wrapper = None) :
+        self.stream = stream_wrapper(fp) if stream_wrapper else fp
---
+++ python/easydump/txt.py
@@ -0,0 +1,35 @@
+class TXTIStream :
+    def __init__(self, i) :
+        self.i = i
+        self.stream_iter = iter(self.__class__.tokens(i))
+    @staticmethod
+    def tokens(io) :
+        for line in io :
+            for token in line.strip().split() :
+                yield token
+    def read(self) :
+        return next(self.stream_iter)
+
+
+class TXTBaseLoader :
+    def load_int(self, _) :
+        return int(self.stream.read())
+    def load_float(self, _) :
+        return float(self.stream.read())
+
+class TXTBaseDumper :
+    def _dump(self, value) :
+        self.stream.write(str(value))
+        self.stream.write(' ')
+    def dump_int(self, value) :
+        self._dump(value)
+    def dump_float(self, value) :
+        self._dump(value)
+
+class TXTLoader (Loader, CompactLoading, TXTBaseLoader) :
+    def __init__(self, fp, stream_wrapper = TXTIStream) :
+        self.stream = stream_wrapper(fp) if stream_wrapper else fp
+
+class TXTDumper (Dumper, CompactDumping, TXTBaseDumper) :
+    def __init__(self, fp, stream_wrapper = None) :
+        self.stream = stream_wrapper(fp) if stream_wrapper else fp
