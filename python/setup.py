584e620d8e8a0421a8874095eb9a3dfd21224318
Jinman Zhao <jinman.zhao@gmail.com>
2018-10-15 13:32:21 -0500

    setup.py

---
+++ python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='easydump',
+    version='0.1.0',
+    packages=['easydump',],
+    license='bsd-3-clause',
+    long_description=open('README.md').read(),
+)
